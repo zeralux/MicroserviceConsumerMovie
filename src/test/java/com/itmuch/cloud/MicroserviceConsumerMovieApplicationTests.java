ee44a0e2cb51d661d110b6ab38545f3b0ccee9dd
USER <USER@MSI>
2019-11-26 12:07:41 +0800

    變更名稱

--- src/test/java/com/itmuch/cloud/MicroserviceConsumerMovieApplicationTests.java
+++ src/test/java/com/itmuch/cloud/MicroserviceConsumerMovieApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroserviceSimpleConsumerMovieApplicationTests {
+class MicroserviceConsumerMovieApplicationTests {
 
 	@Test
 	void contextLoads() {
