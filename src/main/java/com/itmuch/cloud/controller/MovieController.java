e7484ce2cf6e503cb0871f651b129467ff3fac97
USER <USER@MSI>
2019-11-26 12:01:54 +0800

    電影微服務註冊在eureka server

---
+++ src/main/java/com/itmuch/cloud/controller/MovieController.java
@@ -0,0 +1,28 @@
+package com.itmuch.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.itmuch.cloud.pojo.User;
+
+@RestController
+public class MovieController {
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${user.userServiceUrl}")
+	private String userServiceUrl;
+	
+	@GetMapping("/user/{id}")
+	public User findById(@PathVariable Long id) {
+		// User user = this.restTemplate.getForObject("http://localhost:8000/"+id ,User.class);
+		
+		// 雖然可以透過@Value避開hard code問題 , 但事實上當堤共服務者將端點改變後,呼叫服務者仍然必須修改URL
+		User user = this.restTemplate.getForObject(this.userServiceUrl+id ,User.class);
+		return user;
+	}
+}
